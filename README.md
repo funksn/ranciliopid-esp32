dba2733e690c9e03fb8ecfb89ca4f26853f02a23
murmeltier08 <44034211+murmeltier08@users.noreply.github.com>
2021-02-20 20:45:08 +0100

    master
    master

---
+++ README.md
@@ -0,0 +1,39 @@
+# Rancilio PID meets Clevercoffee
+
+## Version
+MASTER VERSION
+Version 2.6.2  (01.02.2021) ADD new Template for the Display
+
+## Hinweise
+Unsere Projektwebseite findet ihr hier: [Rancilio PID Website](http://rancilio-pid.de/).
+
+Wir empfehlen euch gerne unser Handbuch hier in Github zu erst anzuschauen,
+hier geht es zum deutschen Handbuch: [Link](http://manual.rancilio-pid.de/de/)
+
+Unseren Chat für die gemeinsame Zusammenarbeit und Fragen findet ihr hier: [Link](https://chat.rancilio-pid.de)
+
+Anregungen zur Weiterentwicklung der Software werden von uns gerne aufgenommen. User-Input ist immer willkommen und bringt den PID weiter!
+Ihr seid herzlich Willkommen uns gemeinsam bei der Mission zu besseren Espresso zu helfen. 
+
+## Was nach einem Umbau möglich ist
+ * Automatischer Kaltstart für das perfekte Erreichen der Soll-Temperatur innerhalb von 8 bis 12 Minuten (je nach Maschine) 
+ * **NEU:** Shottimer einstellbar, um die Bezugdauer zu sehen.
+ * **NEU:** Aktuell 3 Displaydesgins für die Anzeige, Möglichkeit weitere Designs einfach einzubinden. 
+ * **NEU:** Support für Siliva E Maschinen: Trigger mit definierbaren Intervall für ein Relais zum Überbrücken der Eco-Funktion
+ * Regelung der Brühtemperatur mit einer Genauigkeit von bis zu +/- 0,1°
+ * Steuerung der Brühzeit inkl. Pre-Infusion in der Ausbaustufe „Vollausbau“ möglich
+ * Bedienung über eine Smartphone-App (iOS & Android)
+ * Datenmonitoring über Grafana (webbasiert) 
+ * MQTT (IoT) möglich, um Parameter zu manipulieren. 
+ * Updates der PID-Software sind drahtlos über WLAN möglich
+ * Unsere PID-Software ist OpenSource: für euch kostenfrei und auf persönliche Anforderungen anpassbar
+ * Geringer Platzbedarf, passt problemlos in die meisten kleineren Espressomaschinen
+ * Die serienmäßige Verkabelung der Maschine bleibt erhalten und wird nur erweitert. Die Maschine kann nach dem Umbau problemlos wieder zurückgerüstet werden.
+ * Aktive User-Community im Projektchat mit schnellem Support
+ * Support seit 3 Jahren mit kontinuerlichen Anpassungen
+
+Anregungen zur Weiterentwicklung der Software werden von uns gerne aufgenommen. User-Input ist immer willkommen und bringt den PID weiter!
+Ihr seid herzlich Willkommen uns gemeinsam bei der Mission zu besseren Espresso zu helfen. 
+
+Vielen Dank an jeden einzelnen Unterstützer!
+
---
+++ rancilio-pid/display.ino
@@ -0,0 +1,124 @@
+
+#if (DISPLAY == 1 || DISPLAY == 2)  
+
+    /********************************************************
+     initialize u8g2 display
+    *****************************************************/
+    void u8g2_prepare(void) 
+    {
+        //u8g2.setFont(u8g2_font_6x12_tf);
+        u8g2.setFont(u8g2_font_profont11_tf);
+        //u8g2.setFont(u8g2_font_IPAandRUSLCD_tf);
+        u8g2.setFontRefHeightExtendedText();
+        u8g2.setDrawColor(1);
+        u8g2.setFontPosTop();
+        u8g2.setFontDirection(0);
+        u8g2.setDisplayRotation(DISPALYROTATE);
+    }
+
+    /********************************************************
+     DISPLAY - print message
+    *****************************************************/
+    void displayMessage(String text1, String text2, String text3, String text4, String text5, String text6) 
+    {
+        u8g2.clearBuffer();
+        u8g2.setCursor(0, 0);
+        u8g2.print(text1);
+        u8g2.setCursor(0, 10);
+        u8g2.print(text2);
+        u8g2.setCursor(0, 20);
+        u8g2.print(text3);
+        u8g2.setCursor(0, 30);
+        u8g2.print(text4);
+        u8g2.setCursor(0, 40);
+        u8g2.print(text5);
+        u8g2.setCursor(0, 50);
+        u8g2.print(text6);
+        u8g2.sendBuffer();
+    }
+
+    /********************************************************
+     DISPLAY - print logo and message at boot
+    *****************************************************/
+    void displayLogo(String displaymessagetext, String displaymessagetext2) 
+    {
+        u8g2.clearBuffer();
+        u8g2.drawStr(0, 47, displaymessagetext.c_str());
+        u8g2.drawStr(0, 55, displaymessagetext2.c_str());
+        //Rancilio startup logo
+        if (machineLogo == 1) {
+            u8g2.drawXBMP(41, 2, startLogoRancilio_width, startLogoRancilio_height, startLogoRancilio_bits);
+        } else if (machineLogo == 2) {
+            u8g2.drawXBMP(0, 2, startLogoGaggia_width, startLogoGaggia_height, startLogoGaggia_bits);
+        }
+        u8g2.sendBuffer();
+    }
+
+    /********************************************************
+     DISPLAY - EmergencyStop
+    *****************************************************/
+    void displayEmergencyStop(void) 
+    {
+        u8g2.clearBuffer();
+        u8g2.drawXBMP(0, 0, logo_width, logo_height, logo_bits_u8g2);   //draw temp icon
+        u8g2.setCursor(32, 24);
+        u8g2.print("Ist :  ");
+        u8g2.print(Input, 1);
+        u8g2.print(" ");
+        u8g2.print((char)176);
+        u8g2.print("C");
+        u8g2.setCursor(32, 34);
+        u8g2.print("Soll:  ");
+        u8g2.print(setPoint, 1);
+        u8g2.print(" ");
+        u8g2.print((char)176);
+        u8g2.print("C");
+
+    //draw current temp in icon
+        if (isrCounter < 500)
+        {
+                u8g2.drawLine(9, 48, 9, 5);
+                u8g2.drawLine(10, 48, 10, 4);
+                u8g2.drawLine(11, 48, 11, 3);
+                u8g2.drawLine(12, 48, 12, 4);
+                u8g2.drawLine(13, 48, 13, 5);
+                u8g2.setCursor(32, 4);
+                u8g2.print("HEATING STOPPED");
+        }
+        u8g2.sendBuffer();
+    }
+    void displayShottimer(void) 
+     {
+        if (
+            (
+            (bezugsZeit > 0 && ONLYPID == 1) || // Bezugszeit bei Only PID  
+            (ONLYPID == 0 && brewcounter > 10 && brewcounter <= 42) // oder Bezug bei nicht only PID über brewcounter
+            ) && SHOTTIMER == 1
+            ) // Shotimer muss 1 = True sein und Bezug vorliegen
+        {
+            // Dann Zeit anzeigen
+            u8g2.clearBuffer();
+           // u8g2.drawXBMP(0, 0, logo_width, logo_height, logo_bits_u8g2);   //draw temp icon
+            u8g2.drawXBMP(0, 0, brewlogo_width, brewlogo_height, brewlogo_bits_u8g2);
+            u8g2.setFont(u8g2_font_profont22_tf);
+            u8g2.setCursor(64, 25);
+            u8g2.print(bezugsZeit / 1000, 1);
+            u8g2.setFont(u8g2_font_profont11_tf);
+            u8g2.sendBuffer();
+            
+        }
+        if (SHOTTIMER == 1 && millis() >= bezugszeit_last_Millis && // direkt nach Erstellen von bezugszeit_last_mills (passiert beim ausschalten des Brühschalters, case 43 im Code) soll gestartet werden
+        bezugszeit_last_Millis+brewswitchDelay >= millis() && // soll solange laufen, bis millis() den brewswitchDelay aufgeholt hat, damit kann die Anzeigedauer gesteuert werden
+        bezugszeit_last_Millis < totalbrewtime) // wenn die totalbrewtime automatisch erreicht wird, soll nichts gemacht werden, da sonst falsche Zeit angezeigt wird, da Schalter später betätigt wird als totalbrewtime
+        {
+            u8g2.clearBuffer();
+           u8g2.drawXBMP(0, 0, brewlogo_width, brewlogo_height, brewlogo_bits_u8g2);
+           u8g2.setFont(u8g2_font_profont22_tf);
+           u8g2.setCursor(64, 25);
+           u8g2.print((bezugszeit_last_Millis - startZeit) / 1000, 1);
+           u8g2.setFont(u8g2_font_profont11_tf);
+           u8g2.sendBuffer();
+        }
+    }
+
+#endif
